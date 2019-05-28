50e38b3fcfc94c7e2cbc8b9dc5b8db7911577220
Janakiraman <janakiraman.natarajan@inswit.com>
2019-05-28 18:40:42 +0530

    Logout functionality is implemented

--- src/app/menubar-header/menubar-header.component.ts
+++ src/app/menubar-header/menubar-header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { CommonDataService } from '../services/common-data.service';
+import { UtilService } from '../services/util.service';
 
 @Component({
   selector: 'app-menubar-header',
@@ -10,13 +11,24 @@ export class MenubarHeaderComponent implements OnInit {
   
   isMenuBarShown: boolean;
 
-  constructor(private commonDataService: CommonDataService) {
+  constructor(private utilService: UtilService, private commonDataService: CommonDataService) {
     this.commonDataService.isMenuBarShown.subscribe((value) => {
       this.isMenuBarShown = value;
     });
   }
 
   ngOnInit() {
+  }
+
+  logout() {
+    this.utilService.logout().subscribe(
+      res => {
+        this.utilService.clearCredentials();
+      },
+      error => {
+        this.utilService.clearCredentials();
+      }
+    );
   }
 
 }
