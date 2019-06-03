02096aa1d2254e62bd2918eab9b0274f93fc1b27
Deepen Dhamecha <deepen.dhamecha@twixor.com>
2019-06-03 18:35:29 +0530

    Fixed Field Fill Directive

--- src/app/directives/field-fill.directive.ts
+++ src/app/directives/field-fill.directive.ts
@@ -1,11 +1,32 @@
-import { Directive, ElementRef, Renderer2, HostListener } from '@angular/core';
+import { Directive, ElementRef, Renderer2, HostListener, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Directive({
   selector: '[fieldFill]'
 })
-export class FieldFillDirective {
+export class FieldFillDirective implements OnChanges {
 
   constructor(private el: ElementRef, private ren: Renderer2) { }
+
+  @Input() ngModel;
+ 
+  ngOnInit() {
+    // if(this.el.nativeElement.value != '') {
+    //   this.ren.addClass
+    // } else {
+    //   this.ren.
+    // }
+    // console.log("NGMODEL ", this.ngModel);
+  }
+
+  ngOnChanges(change: SimpleChanges) {
+    if( change.ngModel.currentValue != '' &&
+        change.ngModel.currentValue != null &&
+        change.ngModel.currentValue != undefined) {
+      this.ren.addClass(this.el.nativeElement.parentElement, 'filled');
+    } else {
+      this.ren.removeClass(this.el.nativeElement.parentElement, 'filled');
+    }
+  }
 
   @HostListener('focus', ['$event.target'])
   inputFocus(e) {
@@ -14,6 +35,10 @@ export class FieldFillDirective {
 
   @HostListener('blur', ['$event.target'])
   inputBlur(e) {
-    this.ren.removeClass(e.parentElement, 'filled');
+    if( e.value == '' ||
+        e.value == null ||
+        e.value == undefined) {
+      this.ren.removeClass(e.parentElement, 'filled');
+    }
   }
 }
