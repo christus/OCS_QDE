1c7e525c0aabea083d6a207d46482daeb865bd3a
Sivachandran <siva.chandran@inswit.com>
2019-08-01 18:35:41 +0530

    Slider problem fixed for Applicant and Coapplicant(Communication address, Occupation details)

--- src/app/applicant-dashboard/co-applicant-qde/co-applicant-qde.component.ts
+++ src/app/applicant-dashboard/co-applicant-qde/co-applicant-qde.component.ts
@@ -65,7 +65,7 @@ export class CoApplicantQdeComponent implements OnInit, OnDestroy {
   minValue: number = 1;
   options: Options = {
     floor: 0,
-    ceil: 50,
+    ceil: 100,
     step: 10,
     showTicksValues: false,
     // showSelectionBar: true,
@@ -74,7 +74,17 @@ export class CoApplicantQdeComponent implements OnInit, OnDestroy {
       return  sliderVal + '<b>y</b>';
     }
   };
-  
+  options1: Options = {
+    floor: 1,
+    ceil: 41,
+    step: 5,
+    showTicksValues: false,
+    // showSelectionBar: true,
+    showTicks: true,
+    getLegend: (sliderVal: number): string => {
+      return  sliderVal + '<b>y</b>';
+    }
+  };
   familyOptions: Options = {
     floor: 0,
     ceil: 6,
