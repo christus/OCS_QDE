8a7bcd4db2c1c009bb097845df4af688a752a7fe
christusvalerian <giribabu.yerradla@appiyo.com>
2019-08-13 11:52:20 +0530

    Logout feature in admin panel

--- src/app/admin/admin-panel/admin-panel.component.ts
+++ src/app/admin/admin-panel/admin-panel.component.ts
@@ -1,3 +1,4 @@
+import { UtilService } from 'src/app/services/util.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminPanelComponent implements OnInit {
 
-  constructor() { }
+  constructor(private utilService: UtilService) { }
 
   ngOnInit() {
+  }
+  logout() {
+    this.utilService.clearCredentials();
   }
 
 }
