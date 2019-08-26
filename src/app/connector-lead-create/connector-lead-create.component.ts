38eca1f5a29514150e002c026202d5e3e7e52c9c
Sivachandran <siva.chandran@inswit.com>
2019-08-26 15:21:10 +0530

    Cross button added

--- src/app/connector-lead-create/connector-lead-create.component.ts
+++ src/app/connector-lead-create/connector-lead-create.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { QdeHttpService } from '../services/qde-http.service';
+import { CommonDataService } from '../services/common-data.service';
+import { QdeService } from '../services/qde.service';
 
 @Component({
   selector: 'app-connector-lead-create',
@@ -7,7 +11,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConnectorLeadCreateComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+    private router: Router,
+    private qdeHttp: QdeHttpService,
+    private commonDataService: CommonDataService,
+    private qdeService: QdeService) { 
+      
+    this.commonDataService.changeMenuBarShown(false);
+    this.commonDataService.changeViewFormVisible(false);
+    this.commonDataService.changeLogoutVisible(true)
+  }
 
   ngOnInit() {
   }
