8664e82fe7ffed6f9f0eced82bb0b8431e2577f9
christusvalerian <giribabu.yerradla@appiyo.com>
2019-11-14 20:15:06 +0530

    Fixed Date picker position

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   version: "1.8.6",
-  buildDate: "13-11-2019",
+  buildDate: "14-11-2019",
   production: true,
   projectId: '5928d30eac7811e9a1670242ac110002', // developement
 
